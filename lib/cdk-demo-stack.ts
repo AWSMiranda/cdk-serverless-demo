2a48b32a967522a67949061ad187f2bde8d4ffc4
Miranda.Li <lisirui19881020@gmail.com>
2022-04-11 05:51:42 +0000

    commit app

---
+++ lib/cdk-demo-stack.ts
@@ -0,0 +1,32 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cdk from 'aws-cdk-lib';
+
+export class CdkDemoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const table = new dynamodb.Table(this, 'people', {
+      partitionKey: { name: 'name', type: dynamodb.AttributeType.STRING},
+      tableName: "peopleTable",
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY //remove table if we delete the stack, don't do in PROD!
+    });
+    
+    const createLambda = new lambda.Function(this, 'CreateHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.fromAsset('lambda'),
+      environment: { 'TABLE_NAME': 'peopleTable' },
+      handler: 'createUser.handler'
+    });
+
+    const api = new apigw.RestApi(this, 'apiGateway');
+    const apiCreateInteg = new apigw.LambdaIntegration(createLambda);
+    const apiCreate = api.root.addResource('create');
+    apiCreate.addMethod('POST', apiCreateInteg);
+    table.grantReadWriteData(createLambda);
+  }
+}
